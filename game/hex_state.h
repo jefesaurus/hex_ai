684e1a61cc59f479127d3b08eb2cc39cb16e7306
Geoffrey Lalonde <glalonde@mit.edu>
2017-09-04 01:03:26 -0700

    asdf

--- game/hex_state.h
+++ game/hex_state.h
@@ -1,28 +1,27 @@
 #pragma once
 #include <bitset>
 #include <sstream>
+#include <unordered_set>
 #include "base/logging.h"
-
-enum struct CellState : uint8_t { kEmpty = 0, kWhite = 1, kBlack = 2 };
-
-template <typename Enumeration>
-auto as_underlying(Enumeration const value) ->
-    typename std::underlying_type<Enumeration>::type {
-  return static_cast<typename std::underlying_type<Enumeration>::type>(value);
-}
+#include "game/cell_state.h"
 
 template <int Size>
 class HexState {
  public:
+  static constexpr int kSize = Size;
   static constexpr int kNumCells = Size * Size;
-
-  HexState() {}
 
   void SetCell(int row, int col, CellState val) {
     data_[row * Size + col] = val;
   }
 
+  void SetPiece(int row, int col, PieceType type) {
+    data_[row * Size + col] = CellState(type);
+  }
+
   CellState GetCell(int row, int col) const { return data_[row * Size + col]; }
+
+  CellState GetCell(int index) const { return data_[index]; }
 
   template <typename OStream>
   friend OStream& operator<<(OStream& os, const HexState<Size>& state) {
@@ -39,115 +38,48 @@ class HexState {
 
   const std::array<CellState, kNumCells>& data() const { return data_; }
 
+  static int Index(int row, int col) { return row * Size + col; }
+
+  static int SafeIndex(int row, int col) {
+    if (row < 0 || row >= Size || col < 0 || col >= Size) {
+      return -1;
+    }
+    return Index(row, col);
+  }
+
+  // Precompute indices for the neighbors of each cell.
+  static std::array<std::pair<int, std::array<int, 6>>, kNumCells>
+  ComputeNeighbors() {
+    std::array<std::pair<int, std::array<int, 6>>, kNumCells> out;
+    for (int row = 0; row < Size; ++row) {
+      for (int col = 0; col < Size; ++col) {
+        auto& to_set = out[Index(row, col)];
+        std::vector<int> maybe_neighbors;
+        maybe_neighbors.push_back(SafeIndex(row - 1, col));
+        maybe_neighbors.push_back(SafeIndex(row - 1, col + 1));
+        maybe_neighbors.push_back(SafeIndex(row, col - 1));
+        maybe_neighbors.push_back(SafeIndex(row, col + 1));
+        maybe_neighbors.push_back(SafeIndex(row + 1, col - 1));
+        maybe_neighbors.push_back(SafeIndex(row + 1, col + 1));
+        int count = 0;
+        for (int maybe_neighbor : maybe_neighbors) {
+          if (maybe_neighbor >= 0) {
+            to_set.second[count++] = maybe_neighbor;
+          }
+        }
+        to_set.first = count;
+      }
+    }
+  }
+
  private:
   // Cells are stored row-major order.
   std::array<CellState, kNumCells> data_{};
+
+  // Fake a max size static array.
+  static std::array<std::pair<int, std::array<int, 6>>, kNumCells> neighbors_;
 };
 
-template <typename OStream>
-inline OStream& operator<<(OStream& os, const CellState& val) {
-  switch (val) {
-    case CellState::kEmpty:
-      os << '-';
-      break;
-    case CellState::kWhite:
-      os << 'W';
-      break;
-    case CellState::kBlack:
-      os << 'B';
-      break;
-    default:
-      os << '?';
-      break;
-  }
-  return os;
-}
-
-class FourCells {
- public:
-  FourCells() : data_(0) {}
-
-  template <int Index>
-  void SetCell(CellState val) {
-    data_ = ((data_ & inverse_masks_[Index]) |
-             (as_underlying(val) << shifts_[Index]));
-  }
-
-  void SetCell(std::size_t index, CellState val) {
-    data_ = ((data_ & inverse_masks_[index]) |
-             (as_underlying(val) << shifts_[index]));
-  }
-
-  template <int Index>
-  CellState GetCell() const {
-    return static_cast<CellState>((masks_[Index] & data_) >> shifts_[Index]);
-  }
-
-  CellState GetCell(std::size_t index) const {
-    return static_cast<CellState>((masks_[index] & data_) >> shifts_[index]);
-  }
-
-  const uint8_t& data() const { return data_; }
-
- private:
-  static constexpr std::array<uint8_t, 4> masks_ =
-      std::array<uint8_t, 4>{0b00000011, 0b00001100, 0b00110000, 0b11000000};
-  static constexpr std::array<uint8_t, 4> inverse_masks_ =
-      std::array<uint8_t, 4>{0b11111100, 0b11110011, 0b11001111, 0b00111111};
-  static constexpr std::array<std::size_t, 4> shifts_ =
-      std::array<std::size_t, 4>{0, 2, 4, 6};
-  uint8_t data_;
-};
-
-constexpr int32_t ConstExprCeil(float num) {
-  return (static_cast<float>(static_cast<int32_t>(num)) == num)
-             ? static_cast<int32_t>(num)
-             : static_cast<int32_t>(num) + ((num > 0) ? 1 : 0);
-}
-
-// A version of HexState that packs the bits tightly so it requires bit shifts
-// during access. This appears to be about twice as slow for random read writes.
-// Presumably sequential can be optimized a bit.
-//
-// Takes 1/4 the space however.
 template <int Size>
-class PackedHexState {
- public:
-  static constexpr int kNumCells = Size * Size;
-  static constexpr int kNumChars = ConstExprCeil(kNumCells / 4.0);
-
-  PackedHexState() {}
-
-  void SetCell(int row, int col, CellState val) {
-    int bit_index = row * Size + col;
-    FourCells& cells = data_[bit_index >> 2];
-    cells.SetCell(bit_index % 4, val);
-  }
-
-  CellState GetCell(int row, int col) const {
-    int bit_index = row * Size + col;
-    const FourCells& cells = data_[bit_index >> 2];
-    return cells.GetCell(bit_index % 4);
-  }
-
-  template <typename OStream>
-  friend OStream& operator<<(OStream& os, const PackedHexState<Size>& state) {
-    int count = 0;
-    for (const auto& four_cells : state.data()) {
-      for (int i = 0; i < 4 && count < PackedHexState<Size>::kNumCells; ++i) {
-        if (count % Size == 0) {
-          os << '\n';
-        }
-        os << four_cells.GetCell(i);
-        ++count;
-      }
-    }
-    return os;
-  }
-
-  const std::array<FourCells, kNumChars>& data() const { return data_; }
-
- private:
-  // Cells are stored row-major order.
-  std::array<FourCells, kNumChars> data_{};
-};
+std::array<std::pair<int, std::array<int, 6>>, HexState<Size>::kNumCells>
+    HexState<Size>::neighbors_ = HexState<Size>::ComputeNeighbors();
---
+++ game/packed_hex_state.h
@@ -0,0 +1,96 @@
+
+
+class FourCells {
+ public:
+  FourCells() : data_(0) {}
+
+  template <int Index>
+  void SetCell(CellState val) {
+    data_ = ((data_ & inverse_masks_[Index]) |
+             (as_underlying(val) << shifts_[Index]));
+  }
+
+  void SetCell(std::size_t index, CellState val) {
+    data_ = ((data_ & inverse_masks_[index]) |
+             (as_underlying(val) << shifts_[index]));
+  }
+
+  template <int Index>
+  CellState GetCell() const {
+    return static_cast<CellState>((masks_[Index] & data_) >> shifts_[Index]);
+  }
+
+  CellState GetCell(std::size_t index) const {
+    return static_cast<CellState>((masks_[index] & data_) >> shifts_[index]);
+  }
+
+  const uint8_t& data() const { return data_; }
+
+ private:
+  static constexpr std::array<uint8_t, 4> masks_ =
+      std::array<uint8_t, 4>{0b00000011, 0b00001100, 0b00110000, 0b11000000};
+  static constexpr std::array<uint8_t, 4> inverse_masks_ =
+      std::array<uint8_t, 4>{0b11111100, 0b11110011, 0b11001111, 0b00111111};
+  static constexpr std::array<std::size_t, 4> shifts_ =
+      std::array<std::size_t, 4>{0, 2, 4, 6};
+  uint8_t data_;
+};
+
+constexpr int32_t ConstExprCeil(float num) {
+  return (static_cast<float>(static_cast<int32_t>(num)) == num)
+             ? static_cast<int32_t>(num)
+             : static_cast<int32_t>(num) + ((num > 0) ? 1 : 0);
+}
+
+// A version of HexState that packs the bits tightly so it requires bit shifts
+// during access. This appears to be about twice as slow for random read writes.
+// Presumably sequential can be optimized a bit.
+//
+// Takes 1/4 the space however.
+template <int Size>
+class PackedHexState {
+ public:
+  static constexpr int kSize = Size;
+  static constexpr int kNumCells = Size * Size;
+  static constexpr int kNumChars = ConstExprCeil(kNumCells / 4.0);
+
+  PackedHexState() {}
+
+  void SetCell(int row, int col, CellState val) {
+    int bit_index = row * Size + col;
+    FourCells& cells = data_[bit_index >> 2];
+    cells.SetCell(bit_index % 4, val);
+  }
+
+  CellState GetCell(int row, int col) const {
+    int bit_index = row * Size + col;
+    const FourCells& cells = data_[bit_index >> 2];
+    return cells.GetCell(bit_index % 4);
+  }
+
+  CellState GetCell(int index) const {
+    const FourCells& cells = data_[index >> 2];
+    return cells.GetCell(index % 4);
+  }
+
+  template <typename OStream>
+  friend OStream& operator<<(OStream& os, const PackedHexState<Size>& state) {
+    int count = 0;
+    for (const auto& four_cells : state.data()) {
+      for (int i = 0; i < 4 && count < PackedHexState<Size>::kNumCells; ++i) {
+        if (count % Size == 0) {
+          os << '\n';
+        }
+        os << four_cells.GetCell(i);
+        ++count;
+      }
+    }
+    return os;
+  }
+
+  const std::array<FourCells, kNumChars>& data() const { return data_; }
+
+ private:
+  // Cells are stored row-major order.
+  std::array<FourCells, kNumChars> data_{};
+};
