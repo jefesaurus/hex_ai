989aa5392b76fd0b086200c8af5580bd38c6f1dd
Geoffrey Lalonde <glalonde@mit.edu>
2017-09-04 17:28:28 -0700

    Get NDEBUG

--- base/logging_example.cc
+++ base/logging_example.cc
@@ -3,9 +3,11 @@
 int main(int argc, char** argv) {
   ConfigureEasyLogging(&argc, &argv);
   LOG(INFO) << "yoo";
-  LOG(INFO) << "yoo" << "rofl";
+  LOG(INFO) << "yoo"
+            << "rofl";
   LOG(WARNING) << "whoah there";
   std::vector<std::string> strings = {"yoo", "rofl"};
   LOG(INFO) << strings;
+  CHECK_EQ(2, 2);
   return 0;
 }
