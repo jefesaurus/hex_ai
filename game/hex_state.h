3e32455854cdaa39737165eeb3a4295eda8b510e
Geoffrey Lalonde <glalonde@mit.edu>
2017-09-07 13:43:25 -0700

    360k games per second

--- game/hex_state.h
+++ game/hex_state.h
@@ -1,8 +1,15 @@
 #pragma once
+#include <bitset>
 #include <sstream>
 #include <unordered_set>
 #include "base/logging.h"
 #include "game/cell_state.h"
+
+constexpr int32_t ConstexprCeil(float num) {
+  return (static_cast<float>(static_cast<int32_t>(num)) == num)
+             ? static_cast<int32_t>(num)
+             : static_cast<int32_t>(num) + ((num > 0) ? 1 : 0);
+}
 
 // Lower(row = 0, col = 0)
 //
@@ -26,46 +33,138 @@ class HexState {
  public:
   static constexpr int kSize = Size;
   static constexpr int kNumCells = Size * Size;
+  static constexpr int kMaxNumGroups =
+      ConstexprCeil(kSize / 2.0) * ConstexprCeil(kSize / 2.0);
 
   HexState()
-      : groups_(4, {}), winner_(PieceType::kEmpty) {}
+      : num_horizontal_groups_(2),
+        horizontal_groups_(InitialHorizontalGroups()),
+        num_vertical_groups_(2),
+        vertical_groups_(InitialVerticalGroups()),
+        data_(InitializeData()),
+        winner_(PieceType::kEmpty),
+        empty_spaces_(kNumCells) {}
 
-  bool SetPiece(int index, PieceType type) {
+  void SetPiece(int row, int col, PieceType type) {
+    if (type == PieceType::kHorizontal) {
+      SetHorizontalPiece(Index(row, col));
+    } else {
+      DCHECK_EQ(type, PieceType::kVertical);
+      SetVerticalPiece(Index(row, col));
+    }
+    --empty_spaces_;
+  }
+
+  void SetPiece(int index, PieceType type) {
+    if (type == PieceType::kHorizontal) {
+      SetHorizontalPiece(index);
+    } else {
+      DCHECK_EQ(type, PieceType::kVertical);
+      SetVerticalPiece(index);
+    }
+    --empty_spaces_;
+  }
+
+  void SetHorizontalPiece(int index) {
     auto& new_piece = data_[index];
     DCHECK_EQ(new_piece.GetPieceType(), PieceType::kEmpty);
-    new_piece = CellState(type);
+    new_piece = CellState(PieceType::kHorizontal);
 
-    int current_group = starting_group_[index];
+    const auto& neighbor_mask = neighbor_masks_[index];
 
-
-    int num_neighbors = 0;
-    std::bitset<kNumCells> neighbor_mask;
-
-    for (int neighbor_index : neighbors_[index]) {
-      const auto& neighbor = data_[neighbor_index];
-      if (neighbor.data() & as_underlying(type)) {
-        neighbor_mask[neighbor_index] = true;
-        ++num_neighbors;
-      }
-    }
-
-    std::array<
-    for (auto& group : groups_) {
-      // Neighbor is in this group.
+    // Assign to lowest neighboring group
+    int merge_into = 0;
+    for (; merge_into < num_horizontal_groups_; ++merge_into) {
+      auto& group = horizontal_groups_[merge_into];
       if ((neighbor_mask & group).any()) {
-        // Merge group 
+        // Merge group
+        group[index] = true;
+        break;
       }
     }
 
-    return PropagateNewPiece(index, type);
+    // There was no neighboring group, create a group for it.
+    if (merge_into == num_horizontal_groups_) {
+      horizontal_groups_[num_horizontal_groups_].reset();
+      horizontal_groups_[num_horizontal_groups_++][index] = true;
+      return;
+    }
+
+    // Now it has been merged into a group and we know it has neighbors, so we
+    // need to potentially merge other groups.
+    for (int j = num_horizontal_groups_ - 1; j > merge_into; --j) {
+      auto& group = horizontal_groups_[j];
+      if ((neighbor_mask & group).any()) {
+        // Merge into group j
+        MergeHorizontalGroups(merge_into, j);
+      }
+    }
   }
 
-  void MergeGroups(int a, int b) {
-    groups_[a] |= groups_[b];
+  void SetVerticalPiece(int index) {
+    auto& new_piece = data_[index];
+    DCHECK_EQ(new_piece.GetPieceType(), PieceType::kEmpty);
+    new_piece = CellState(PieceType::kVertical);
+
+    const auto& neighbor_mask = neighbor_masks_[index];
+
+    // Assign to lowest neighboring group
+    int merge_into = 0;
+    for (; merge_into < num_vertical_groups_; ++merge_into) {
+      auto& group = vertical_groups_[merge_into];
+      if ((neighbor_mask & group).any()) {
+        // Merge group
+        group[index] = true;
+        break;
+      }
+    }
+
+    // There was no neighboring group, create a group for it.
+    if (merge_into == num_vertical_groups_) {
+      vertical_groups_[num_vertical_groups_].reset();
+      vertical_groups_[num_vertical_groups_++][index] = true;
+      return;
+    }
+
+    // Now it has been merged into a group and we know it has neighbors, so we
+    // need to potentially merge other groups.
+    for (int j = num_vertical_groups_ - 1; j > merge_into; --j) {
+      auto& group = vertical_groups_[j];
+      if ((neighbor_mask & group).any()) {
+        // Merge into group j
+        MergeVerticalGroups(merge_into, j);
+      }
+    }
   }
 
-  bool SetPiece(int row, int col, PieceType type) {
-    return SetPiece(Index(row, col), type);
+  void MergeVerticalGroups(int a, int b) {
+    DCHECK_LT(a, b);
+    if (a == 0 && b == 1) {
+      winner_ = PieceType::kVertical;
+    } else {
+      vertical_groups_[a] |= vertical_groups_[b];
+      // Erase b
+      DCHECK_GT(num_vertical_groups_, 2);
+      --num_vertical_groups_;
+      if (b < num_vertical_groups_) {
+        vertical_groups_[b] = vertical_groups_[num_vertical_groups_];
+      }
+    }
+  }
+
+  void MergeHorizontalGroups(int a, int b) {
+    DCHECK_LT(a, b);
+    if (a == 0 && b == 1) {
+      winner_ = PieceType::kHorizontal;
+    } else {
+      horizontal_groups_[a] |= horizontal_groups_[b];
+      // Erase b
+      DCHECK_GT(num_horizontal_groups_, 2);
+      --num_horizontal_groups_;
+      if (b < num_horizontal_groups_) {
+        horizontal_groups_[b] = horizontal_groups_[num_horizontal_groups_];
+      }
+    }
   }
 
   bool GameIsOver() { return winner_ != PieceType::kEmpty; }
@@ -75,7 +174,6 @@ class HexState {
   CellState GetCell(int row, int col) const { return data_[row * Size + col]; }
 
   CellState GetCell(int index) const { return data_[index]; }
-
 
   template <typename OStream>
   friend OStream& operator<<(OStream& os, const HexState<Size>& state) {
@@ -145,10 +243,21 @@ class HexState {
   }
 
   static int SafeIndex(int row, int col) {
-    if (row < 0 || row >= Size || col < 0 || col >= Size) {
+    if (col < 0) {
+      // Horizontal lower
       return kNumCells;
+    } else if (col >= Size) {
+      // Horizontal upper
+      return kNumCells + 1;
+    } else if (row < 0) {
+      // Vertical lower
+      return kNumCells + 2;
+    } else if (row >= Size) {
+      // Vertical upper
+      return kNumCells + 3;
+    } else {
+      return Index(row, col);
     }
-    return Index(row, col);
   }
 
   // Precompute indices for the neighbors of each cell.
@@ -168,49 +277,57 @@ class HexState {
     return out;
   }
 
-  static std::array<std::array<int, 6>, kNumCells> ComputeNeighbors() {
-    std::array<std::array<int, 3>, kNumCells> out;
-    for (int row = 0; row < Size; ++row) {
-      for (int col = 0; col < Size; ++col) {
-        const int index = Index(row, col);
-        {
-          auto& group_index = out[index][as_underlying(PieceType::kHorizontal)];
-          if (col <= 0) {
-            group_index = 0;
-          } else if (col >= Size - 1) {
-            group_index = 1;
-          } else {
-            group_index = -1;
-          }
-        }
-        {
-          auto& group_index = out[index][as_underlying(PieceType::kVertical)];
-          new_piece = CellState(PieceType::kVertical);
-          if (row <= 0) {
-            group_index = 2;
-          } else if (row >= Size - 1) {
-            group_index = 3;
-          } else {
-            group_index = -1;
-          }
-        }
+  static std::array<std::bitset<kNumCells + 4>, kNumCells> ComputeNeighborMasks(
+      const std::array<std::array<int, 6>, kNumCells>& neighbors) {
+    std::array<std::bitset<kNumCells + 4>, kNumCells> out;
+    for (int i = 0; i < kNumCells; ++i) {
+      for (const auto& neighbor : neighbors[i]) {
+        out[i][neighbor] = true;
       }
     }
     return out;
   }
 
+  static std::array<std::bitset<kNumCells + 4>, kMaxNumGroups>
+  InitialHorizontalGroups() {
+    std::array<std::bitset<kNumCells + 4>, kMaxNumGroups> groups;
+    groups[0][kNumCells] = true;
+    groups[1][kNumCells + 1] = true;
+    return groups;
+  }
+
+  static std::array<std::bitset<kNumCells + 4>, kMaxNumGroups>
+  InitialVerticalGroups() {
+    std::array<std::bitset<kNumCells + 4>, kMaxNumGroups> groups;
+    groups[0][kNumCells + 2] = true;
+    groups[1][kNumCells + 3] = true;
+    return groups;
+  }
+
+  static std::array<CellState, kNumCells + 4> InitializeData() {
+    std::array<CellState, kNumCells + 4> data{};
+    data[kNumCells] = CellState(PieceType::kHorizontal);
+    data[kNumCells + 1] = CellState(PieceType::kHorizontal);
+    data[kNumCells + 2] = CellState(PieceType::kVertical);
+    data[kNumCells + 3] = CellState(PieceType::kVertical);
+    return data;
+  }
+
  private:
   static std::array<std::array<int, 6>, kNumCells> neighbors_;
-  static std::array<std::array<int, 3>, kNumCells> starting_group_;
+  static std::array<std::bitset<kNumCells + 4>, kNumCells> neighbor_masks_;
 
-  std::array<std::bitset<kNumCells>, kNumCells> groups_;
+  int num_horizontal_groups_;
+  std::array<std::bitset<kNumCells + 4>, kMaxNumGroups> horizontal_groups_;
+  int num_vertical_groups_;
+  std::array<std::bitset<kNumCells + 4>, kMaxNumGroups> vertical_groups_;
 
-
-  // Cells are stored row-major order, with one more on the end as a sentinel.
-  std::array<CellState, kNumCells + 1> data_{};
+  // Cells are stored row-major order, with a sentinel for each side.
+  std::array<CellState, kNumCells + 4> data_;
 
   PieceType winner_;
 
+  int empty_spaces_;
 };
 
 template <int Size>
@@ -218,5 +335,7 @@ std::array<std::array<int, 6>, HexState<Size>::kNumCells>
     HexState<Size>::neighbors_ = HexState<Size>::ComputeNeighbors();
 
 template <int Size>
-std::array<std::array<CellState, 3>, HexState<Size>::kNumCells>
-    HexState<Size>::cell_templates_ = HexState<Size>::ComputeCellTemplates();
+std::array<std::bitset<HexState<Size>::kNumCells + 4>,
+           HexState<Size>::kNumCells>
+    HexState<Size>::neighbor_masks_ =
+        HexState<Size>::ComputeNeighborMasks(HexState<Size>::neighbors_);
--- game/hex_state_example.cc
+++ game/hex_state_example.cc
@@ -1,7 +1,20 @@
 #include "base/init.h"
 #include "game/hex_state.h"
 
-void HorizontalPlayout() {
+void RandomPlayout1() {
+  HexState<5> state;
+  std::vector<int> moves{11, 19, 20, 10, 24, 21, 14, 23, 5,  12, 8, 4, 22,
+                         13, 18, 3,  0,  15, 7,  2,  1,  16, 17, 9, 6};
+  std::array<PieceType, 2> players{PieceType::kHorizontal,
+                                   PieceType::kVertical};
+  for (int i = 0; i < moves.size(); ++i) {
+    state.SetPiece(moves[i], players[i % 2]);
+  }
+  CHECK(state.GameIsOver()) << state;
+  CHECK_EQ(state.Winner(), PieceType::kVertical);
+}
+
+void HorizontalPlayout1() {
   HexState<5> state;
   state.SetPiece(0, 0, PieceType::kHorizontal);
   CHECK(!state.GameIsOver());
@@ -12,7 +25,37 @@ void HorizontalPlayout() {
   state.SetPiece(0, 3, PieceType::kHorizontal);
   CHECK(!state.GameIsOver());
   state.SetPiece(0, 4, PieceType::kHorizontal);
-  CHECK(state.GameIsOver()) << state;
+  CHECK(state.GameIsOver());  // << state;
+  CHECK_EQ(state.Winner(), PieceType::kHorizontal);
+}
+
+void HorizontalPlayout2() {
+  HexState<5> state;
+  state.SetPiece(0, 4, PieceType::kHorizontal);
+  CHECK(!state.GameIsOver());
+  state.SetPiece(0, 3, PieceType::kHorizontal);
+  CHECK(!state.GameIsOver());
+  state.SetPiece(0, 1, PieceType::kHorizontal);
+  CHECK(!state.GameIsOver());
+  state.SetPiece(0, 0, PieceType::kHorizontal);
+  CHECK(!state.GameIsOver());
+  state.SetPiece(0, 2, PieceType::kHorizontal);
+  CHECK(state.GameIsOver());  // << state;
+  CHECK_EQ(state.Winner(), PieceType::kHorizontal);
+}
+
+void HorizontalPlayout3() {
+  HexState<5> state;
+  state.SetPiece(0, 0, PieceType::kHorizontal);
+  CHECK(!state.GameIsOver());
+  state.SetPiece(0, 1, PieceType::kHorizontal);
+  CHECK(!state.GameIsOver());
+  state.SetPiece(0, 2, PieceType::kHorizontal);
+  CHECK(!state.GameIsOver());
+  state.SetPiece(0, 3, PieceType::kHorizontal);
+  CHECK(!state.GameIsOver());
+  state.SetPiece(0, 4, PieceType::kHorizontal);
+  CHECK(state.GameIsOver());  // << state;
   CHECK_EQ(state.Winner(), PieceType::kHorizontal);
 }
 
@@ -27,7 +70,7 @@ void VerticalPlayout() {
   state.SetPiece(3, 0, PieceType::kVertical);
   CHECK(!state.GameIsOver());
   state.SetPiece(4, 0, PieceType::kVertical);
-  CHECK(state.GameIsOver()) << state;
+  CHECK(state.GameIsOver());  // << state;
   CHECK_EQ(state.Winner(), PieceType::kVertical);
 }
 
@@ -44,7 +87,10 @@ void TestPrint() {
 
 int main(int argc, char** argv) {
   Init(&argc, &argv);
-  HorizontalPlayout();
+  RandomPlayout1();
+  HorizontalPlayout1();
+  HorizontalPlayout2();
+  HorizontalPlayout3();
   VerticalPlayout();
   TestPrint();
   return 0;
