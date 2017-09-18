c064404c77aea89d96da4b899adeeb170da3fd23
Geoffrey Lalonde <glalonde@mit.edu>
2017-09-18 02:22:43 -0700

    congratulations, you played yourself

--- mcts/tree_test.cc
+++ mcts/tree_test.cc
@@ -7,10 +7,17 @@ DEFINE_int32(num_trials, 100, "Num trials");
 int main(int argc, char** argv) {
   Init(&argc, &argv);
   HexState<11> game;
-  game.SetPiece(1, 1);
-  HexTree<11> tree(game);
-  for (int i = 0; i < FLAGS_num_trials; ++i) {
-    tree.RunSimulation();
+  while (!game.GameIsOver()) {
+    HexTree<11> tree(game);
+    for (int i = 0; i < FLAGS_num_trials; ++i) {
+      tree.RunSimulation();
+    }
+    const auto best_move = tree.GetBestMove();
+    LOG(INFO) << game.ToMove() << " plays: " << best_move.first
+              << " thinks it will win: " << best_move.second;
+    game.SetPiece(best_move.first);
+    LOG(INFO) << game;
   }
+  LOG(INFO) << "Winner is: " << game.Winner();
   return 0;
 }
