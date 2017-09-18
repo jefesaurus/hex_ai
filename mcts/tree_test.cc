eafdec3dda8e6b87afda4bfb73c6c69282ef7ac5
Geoffrey Lalonde <glalonde@mit.edu>
2017-09-18 10:30:16 -0700

    more games

--- mcts/tree_test.cc
+++ mcts/tree_test.cc
@@ -2,22 +2,25 @@
 #include "base/wall_timer.h"
 #include "mcts/mcts.h"
 
-DEFINE_int32(num_trials, 100, "Num trials");
+DEFINE_int32(num_trials, 100, "Num games to play out.");
+DEFINE_int32(num_iters, 100, "Num tree iterations for each move.");
 
 int main(int argc, char** argv) {
   Init(&argc, &argv);
-  HexState<11> game;
-  while (!game.GameIsOver()) {
-    HexTree<11> tree(game);
-    for (int i = 0; i < FLAGS_num_trials; ++i) {
-      tree.RunSimulation();
+  std::unordered_map<int, int> winners;
+  for (int i = 0; i < FLAGS_num_trials; ++i) {
+    HexState<11> game;
+    while (!game.GameIsOver()) {
+      HexTree<11> tree(game);
+      for (int j = 0; j < FLAGS_num_iters; ++j) {
+        tree.RunSimulation();
+      }
+      const auto best_move = tree.GetBestMove();
+      game.SetPiece(best_move.first);
     }
-    const auto best_move = tree.GetBestMove();
-    LOG(INFO) << game.ToMove() << " plays: " << best_move.first
-              << " thinks it will win: " << best_move.second;
-    game.SetPiece(best_move.first);
-    LOG(INFO) << game;
+    LOG(INFO) << "Game " << i << " result: winner is " << game.Winner();
+    winners[as_underlying(game.Winner())]++;
   }
-  LOG(INFO) << "Winner is: " << game.Winner();
+  LOG(INFO) << winners;
   return 0;
 }
