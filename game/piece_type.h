f611e2d0cff8445fb9c7f9c494d3432e283785ae
Geoffrey Lalonde <glalonde@mit.edu>
2017-09-24 00:45:41 -0700

    comments and printout

--- game/piece_type.h
+++ game/piece_type.h
@@ -1,18 +1,7 @@
 #pragma once
-/*
-
-                           Top       Upper Corner
-                         __________
-                        /         /
-              Left     /         /
-                      /         /  Right
-                     /_________/
-
-       Lower Corner    Bottom
-*/
-
-// Horizontal pieces are trying to connect horizontally, Vertical pieces are
-// trying to connect vertically
+// The two players are "Horizontal" and "Vertical" instead of white and black,
+// where Horizontal is trying to connect column 0 to column board size, and
+// vertical is trying to connect row 0 to row board size. Horizontal goes first.
 enum struct PieceType : uint8_t {
   kEmpty = 0,
   kHorizontal = 1,
@@ -20,12 +9,15 @@ enum struct PieceType : uint8_t {
   kSize = 3
 };
 
+// Get the underlying uint8_t/char byte that the enum value is represented with.
+// This is good for using the enum directly as an index.
 template <typename Enumeration>
 constexpr auto as_underlying(Enumeration const value) ->
     typename std::underlying_type<Enumeration>::type {
   return static_cast<typename std::underlying_type<Enumeration>::type>(value);
 }
 
+// Steam operator for pretty printing PieceType
 template <typename OStream>
 inline OStream& operator<<(OStream& os, const PieceType& val) {
   switch (val) {
@@ -45,6 +37,7 @@ inline OStream& operator<<(OStream& os, const PieceType& val) {
   return os;
 }
 
+// Flips Horizontal and Vertical, but empty stays the same.
 static constexpr std::array<PieceType, 3> flip_index = {
     PieceType::kEmpty, PieceType::kVertical, PieceType::kHorizontal};
 constexpr PieceType FlipPlayer(const PieceType& piece_type) {
