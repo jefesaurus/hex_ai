becbd32d0cbd22d80ba13dfd3fca27f053880377
Geoffrey Lalonde <lalondegeoffrey@gmail.com>
2017-10-12 00:27:13 -0700

    python proto file tests

--- proto/proto_file.py
+++ proto/proto_file.py
@@ -1,3 +1,5 @@
+from google.protobuf.text_format import MessageToString, Merge
+
 def WriteBinaryProto(message, filename):
 	bytes = message.SerializeToString()
 	with open(filename, 'wb') as f:
@@ -5,7 +7,18 @@ def WriteBinaryProto(message, filename):
 
 def ReadBinaryProto(message_class, filename):
 	# Copy the message class so we can parse into it.
-	message = message_class()
+	message = type(message_class)()
 	with open(filename, 'rb') as f:
 		message.ParseFromString(f.read())
+	return message
+
+def WriteTextProto(message, filename):
+	with open(filename, 'w') as f:
+		f.write(MessageToString(message))
+
+def ReadTextProto(message_class, filename):
+	# Copy the message class so we can parse into it.
+	message = type(message_class)()
+	with open(filename, 'r') as f:
+		Merge(f.read(), message)
 	return message
--- proto/proto_file_py_test.py
+++ proto/proto_file_py_test.py
@@ -1,11 +1,38 @@
 import unittest
 import tempfile
 
+from proto import proto_file 
 from proto import test_pb2
 
 class BinaryProtoTest(unittest.TestCase):
   def test_write(self):
+    message = test_pb2.HelloProto()
+    message.number = 5
+    message.vals.append(0)
+    message.vals.append(1)
+    message.vals.append(2)
+    message.vals.append(3)
+    message.vals.append(4)
+    message.label = 'hello'
     tmp = tempfile.NamedTemporaryFile().name
+    proto_file.WriteBinaryProto(message, tmp)
+    message_back = proto_file.ReadBinaryProto(message, tmp)
+    self.assertEqual(message_back.number, message.number)
+
+class TextProtoTest(unittest.TestCase):
+  def test_write(self):
+    message = test_pb2.HelloProto()
+    message.number = 5
+    message.vals.append(0)
+    message.vals.append(1)
+    message.vals.append(2)
+    message.vals.append(3)
+    message.vals.append(4)
+    message.label = 'hello'
+    tmp = tempfile.NamedTemporaryFile().name
+    proto_file.WriteTextProto(message, tmp)
+    message_back = proto_file.ReadTextProto(message, tmp)
+    self.assertEqual(message_back.number, message.number)
 
 if __name__ == '__main__':
     unittest.main()
