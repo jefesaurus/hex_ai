87f74fded720ececc5d5c030d4a8b1a6a79b667e
Geoffrey Lalonde <glalonde@mit.edu>
2017-10-16 00:24:59 -0700

    asdf

--- docker/bash_append.sh
+++ docker/bash_append.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # This is sourced in the .bashrc within the container during "login"
 export TERM=xterm
-DIR=$(dirname ${BASH_SOURCE[0]})
-REPO_ROOT=$(dirname $DIR)
+THIS_SCRIPT_DIR=$(dirname ${BASH_SOURCE[0]})
+REPO_ROOT=$(dirname $THIS_SCRIPT_DIR)
 BAZEL_OUTPUT_DIR=$REPO_ROOT/.bazel_output
 alias bazel="bazel --output_base=$BAZEL_OUTPUT_DIR"
 export REPO_ROOT
